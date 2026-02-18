50d5c69338f2b82703573a93d49419ac9197243f
nikki <cozercaniegirl143@mail.ru>
2026-02-19 00:01:24 +0300

    psql

--- todo/server/db.js
+++ todo/server/db.js
@@ -1,16 +1,16 @@
 import pg from 'pg';
 const { Pool } = pg;
 
-const DB_USER = process.env.DB_USER;
-const DB_PORT = process.env.DB_HOST;
-const HOST = process.env.HOST;
-const DB_PSW = process.env.DB_PSW;
-const DB_NAME = process.env.DB_NAME;
+const PGUSER = process.env.DB_USER;
+const PGPORT = process.env.PGPORT;
+const PGHOST = process.env.PGHOST;
+const PGPASSWORD = process.env.DB_PSW;
+const PGDATABASE = process.env.DB_NAME;
 
 export const pool = new Pool({
-    user: DB_USER,
-    host: HOST,
-    database: DB_NAME,
-    password: DB_PSW,
-    port: DB_PORT,
+    user: PGUSER,
+    host: PGHOST,
+    database: PGDATABASE,
+    password: PGPASSWORD,
+    port: PGPORT,
 });
