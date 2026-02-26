bbfd38170b995606544c5494203bd475057f3e57
nikki <cozercaniegirl143@mail.ru>
2026-02-26 13:20:09 +0300

    check todo 11x

--- server/post.controller.js
+++ server/post.controller.js
@@ -62,7 +62,7 @@ export default class taskController {
         const task_id = task_url.at(-1);
 
         try {
-            # TODO: huesosssssssssssssss
+            # TODO: AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
             if (typeof task_id !== 'number') {
                 throw new Error('ID_NOT_VALID')
             }
